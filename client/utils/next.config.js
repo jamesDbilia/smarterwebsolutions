9760e582e0d2e28fb67d0a7b40af4d74ef024f98
jamesDbilia <james@dbilia.com>
2023-09-01 00:17:00 -0400

    fix: build

--- client/utils/next.config.js
+++ client/utils/next.config.js
@@ -1,17 +1,7 @@
 const withImages = require('next-optimized-images');
 const withCss = require('@zeit/next-css');
-const withReactSvg = require('next-react-svg');
-const path = require('path');
 
 module.exports = withImages(
-  withCss(
-    withReactSvg({
-      include: path.resolve(__dirname, 'public/svg'),
-      webpack(config, options) {
-        // Add any additional webpack configurations if necessary
-        return config;
-      },
-      trailingSlash: true,
-    })
-  )
+ withCss(),
+  { trailingSlash: true }
 );
